98e6c8ac8d7e483cf4413879ef14633668c2eed3
gabriel-reis-silva <gabriel.reis1945@gmail.com>
2021-04-20 10:15:12 -0300

    iniciando projeto

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import PersonList from './PersonList';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <PersonList />
   </React.StrictMode>,
   document.getElementById('root')
 );
